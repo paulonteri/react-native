88b111bdbd95dc45cf307a24b7afd67321f3b094
Paul Onteri <onteripaul@gmail.com>
2020-09-15 13:44:05 +0300

    simple navigation

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View, TextInput, Button } from 'react-native';
 import { styles, formViewStyles } from './styles';
 
-export default function Login() {
+export default function Login(props) {
   return (
     <View style={styles.container}>
       <View style={styles.containerText}>
@@ -20,7 +20,10 @@ export default function Login() {
           <Text style={styles.formText}>Password</Text>
           <TextInput style={styles.formInput} />
         </View>
-        <Button title="Login" />
+        <Button
+          title="Login"
+          onPress={() => props.navigation.navigate('Home')}
+        />
       </View>
       {/* <--- end formView ---> */}
     </View>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <View>
       <Text>Home Screen</Text>
+      <Button
+        style={{ margin: 20 }}
+        title="Login"
+        onPress={() => props.navigation.navigate('Login')}
+      />
+      <Button
+        style={{ margin: 20 }}
+        title="Go back"
+        onPress={() => props.navigation.goBack()}
+      />
     </View>
   );
 }
